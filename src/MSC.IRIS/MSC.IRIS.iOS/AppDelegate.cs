a4b40863bfb00a89a81f4e77961b5cd6499ce5aa
Denys Linkov <dlinkov@virtlogic.ca>
2016-10-22 15:20:17 -0400

    iOS Push Works
    Added push notifications for iOS

--- src/MSC.IRIS/MSC.IRIS.iOS/AppDelegate.cs
+++ src/MSC.IRIS/MSC.IRIS.iOS/AppDelegate.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 using Foundation;
 using UIKit;
-
+using WindowsAzure.Messaging;
 namespace MSC.IRIS.iOS
 {
 	// The UIApplicationDelegate for the application. This class is responsible for launching the 
@@ -22,8 +22,8 @@ namespace MSC.IRIS.iOS
 		//
 		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
 		{
-			global::Xamarin.Forms.Forms.Init ();
-			LoadApplication (new MSC.IRIS.App ());
+			global::Xamarin.Forms.Forms.Init();
+			LoadApplication(new MSC.IRIS.App());
 
 			// if the device is ios9 + enable push notifications
 			if (UIDevice.CurrentDevice.CheckSystemVersion(9, 0))
@@ -39,10 +39,95 @@ namespace MSC.IRIS.iOS
 
 
 
-			return base.FinishedLaunching (app, options);
+			return base.FinishedLaunching(app, options);
 		}
 
+		public override void FailedToRegisterForRemoteNotifications(UIApplication application, NSError error)
+		{
+			new UIAlertView("Error registering push notifications", error.LocalizedDescription, null, "OK", null).Show();
+		}
 
+		private SBNotificationHub Hub { get; set; }
+		public override void ReceivedRemoteNotification(UIApplication application, NSDictionary userInfo)
+		{
+			ProcessNotification(userInfo, false);
+		}
+		void ProcessNotification(NSDictionary options, bool fromFinishedLaunching)
+		{
+			// Check to see if the dictionary has the aps key.  This is the notification payload you would have sent
+			if (null != options && options.ContainsKey(new NSString("aps")))
+			{
+				//Get the aps dictionary
+				NSDictionary aps = options.ObjectForKey(new NSString("aps")) as NSDictionary;
+
+				string alert = string.Empty;
+
+				//Extract the alert text
+				// NOTE: If you're using the simple alert by just specifying
+				// "  aps:{alert:"alert msg here"}  ", this will work fine.
+				// But if you're using a complex alert with Localization keys, etc.,
+				// your "alert" object from the aps dictionary will be another NSDictionary.
+				// Basically the JSON gets dumped right into a NSDictionary,
+				// so keep that in mind.
+				if (aps.ContainsKey(new NSString("alert")))
+					alert = (aps[new NSString("alert")] as NSString).ToString();
+
+				//If this came from the ReceivedRemoteNotification while the app was running,
+				// we of course need to manually process things like the sound, badge, and alert.
+				if (!fromFinishedLaunching)
+				{
+					//Manually show an alert
+					if (!string.IsNullOrEmpty(alert))
+					{
+						UIAlertView avAlert = new UIAlertView("Notification", alert, null, "OK", null);
+						avAlert.Show();
+					}
+				}
+			}
+		}
+		public override void RegisteredForRemoteNotifications(UIApplication application, NSData deviceToken)
+		{
+			// Get current device token
+			var DeviceToken = deviceToken.Description;
+			if (!string.IsNullOrWhiteSpace(DeviceToken))
+			{
+				DeviceToken = DeviceToken.Trim('<').Trim('>');
+			}
+
+			// Get previous device token
+			var oldDeviceToken = NSUserDefaults.StandardUserDefaults.StringForKey("PushDeviceToken");
+
+			// Has the token changed?
+			if (string.IsNullOrEmpty(oldDeviceToken) || !oldDeviceToken.Equals(DeviceToken))
+			{
+				//TODO: Put your own logic here to notify your server that the device token has changed/been created!
+			}
+
+			// Save new device token 
+			NSUserDefaults.StandardUserDefaults.SetString(DeviceToken, "PushDeviceToken");
+
+
+
+
+
+			Hub = new SBNotificationHub(Constants.ConnectionString, Constants.NotificationHubPath);
+
+			Hub.UnregisterAllAsync(deviceToken, (error) =>
+			{
+				if (error != null)
+				{
+					Console.WriteLine("Error calling Unregister: {0}", error.ToString());
+					return;
+				}
+
+				NSSet tags = null; // create tags if you want
+				Hub.RegisterNativeAsync(deviceToken, tags, (errorCallback) =>
+				{
+					if (errorCallback != null)
+						Console.WriteLine("RegisterNativeAsync error: " + errorCallback.ToString());
+				});
+			});
+		}
 
 	}
 }
---
+++ src/MSC.IRIS/MSC.IRIS/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace MSC.IRIS
+{
+	public static class Constants
+	{
+		//android
+		public const string SenderID = "<GoogleProjectNumber>"; // Google API Project Number
+		public const string ListenConnectionString = "<Listen connection string>";
+		public const string NotificationHubName = "<hub name>";
+
+		//ios
+		public const string ConnectionString = "Endpoint=sb://missingchildren.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=6w1EEBnt1MfFB2oRHd7fG2V8cnw/sdeZQ9lDxSUqhZk=";
+		public const string NotificationHubPath = "MissingChildrenHub";
+	}
+}
