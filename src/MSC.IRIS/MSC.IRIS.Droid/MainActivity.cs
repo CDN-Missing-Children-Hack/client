070140c6d17784bf41632b15313ff0421253c93b
Denys Linkov <dlinkov@virtlogic.ca>
2016-10-22 17:08:40 -0400

    Android push
    Added android push

--- src/MSC.IRIS/MSC.IRIS.Droid/MainActivity.cs
+++ src/MSC.IRIS/MSC.IRIS.Droid/MainActivity.cs
@@ -6,18 +6,46 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Util;
+using Gcm.Client;
+
+
 
 namespace MSC.IRIS.Droid
 {
 	[Activity (Label = "MSC.IRIS", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
 	{
+		public static MainActivity instance;
+
 		protected override void OnCreate (Bundle bundle)
 		{
-			base.OnCreate (bundle);
+
+			instance = this;
+
+			base.OnCreate(bundle);
+
+			// Set your view from the "main" layout resource
+			//SetContentView(Resource.Layout.Main);
+
+			// Get your button from the layout resource,
+			// and attach an event to it
+			//Button button = FindViewById<Button>(Resource.Id.myButton);
+
+			RegisterWithGCM();
 
 			global::Xamarin.Forms.Forms.Init (this, bundle);
 			LoadApplication (new MSC.IRIS.App ());
+		}
+		private void RegisterWithGCM()
+		{
+			// Check to ensure everything's set up right
+			GcmClient.CheckDevice(this);
+			GcmClient.CheckManifest(this);
+
+			// Register for push notifications
+			Log.Info("MainActivity", "Registering...");
+			GcmClient.Register(this, Constants.SenderID);
 		}
 	}
 }
--- src/MSC.IRIS/MSC.IRIS/Constants.cs
+++ src/MSC.IRIS/MSC.IRIS/Constants.cs
@@ -4,9 +4,7 @@ namespace MSC.IRIS
 	public static class Constants
 	{
 		//android
-		public const string SenderID = "<GoogleProjectNumber>"; // Google API Project Number
-		public const string ListenConnectionString = "<Listen connection string>";
-		public const string NotificationHubName = "<hub name>";
+		public const string SenderID = "395486619176"; // Google API Project Number
 
 		//ios
 		public const string ConnectionString = "Endpoint=sb://missingchildren.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=6w1EEBnt1MfFB2oRHd7fG2V8cnw/sdeZQ9lDxSUqhZk=";
