ba39ca121265ab463a31ed9770bc1b29f95a1cac
Mohammed Eseifan <m.eseifansilicon@gmail.com>
2016-10-23 11:03:35 -0400

    Merged newest changes with android and iOS push notifications

--- src/MSC.IRIS/MSC.IRIS.Droid/MainActivity.cs
+++ src/MSC.IRIS/MSC.IRIS.Droid/MainActivity.cs
@@ -6,21 +6,42 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Util;
+using Gcm.Client;
 
 namespace MSC.IRIS.Droid
 {
 	[Activity (Label = "MSC.IRIS", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
+
+		public static MainActivity instance;
+
 		protected override void OnCreate (Bundle bundle)
 		{
+			instance = this;
+
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate (bundle);
 
+			RegisterWithGCM();
+
+
 			global::Xamarin.Forms.Forms.Init (this, bundle);
 			LoadApplication (new MSC.IRIS.App ());
+		}
+
+		private void RegisterWithGCM()
+		{
+			// Check to ensure everything's set up right
+			GcmClient.CheckDevice(this);
+			GcmClient.CheckManifest(this);
+
+			// Register for push notifications
+			Log.Info("MainActivity", "Registering...");
+			GcmClient.Register(this, Constants.SenderID);
 		}
 	}
 }
---
+++ src/MSC.IRIS/MSC.IRIS/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace MSC.IRIS
+{
+	public static class Constants
+	{
+		//android
+		public const string SenderID = "395486619176"; // Google API Project Number
+
+		//ios
+		public const string ConnectionString = "Endpoint=sb://missingchildren.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=6w1EEBnt1MfFB2oRHd7fG2V8cnw/sdeZQ9lDxSUqhZk=";
+		public const string NotificationHubPath = "MissingChildrenHub";
+	}
+}
